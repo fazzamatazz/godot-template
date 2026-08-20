52d7cc2842e1970977ce91a7130f768ada6f5d07
rfarr <9212000+fazzamatazz@users.noreply.github.com>
2026-08-20 16:10:14 +0100

    add loading scene to game

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,21 +1,42 @@
 class_name Game extends Node
 
-@export var main_scene: PackedScene
+@onready var _loading_screen: Control = %LoadingScreen
+@onready var _loading_sprite: Sprite2D = %Sprite2D
 
-var main: MainScene
+@export var _main_scene: PackedScene
+
+var _is_loading: bool = true
+var _main: MainScene
 
 
 func _ready() -> void:
 	start_main_scene()
 
 
+func _process(delta: float) -> void:
+	if _is_loading:
+		_loading_sprite.rotation += delta * 4.0
+
+
+func _show_loading_screen() -> void:
+	_loading_screen.show()
+	_is_loading = true
+
+
+func _hide_loading_screen() -> void:
+	_loading_screen.hide()
+	_is_loading = false
+	_loading_sprite.rotation = 0.0
+
+
 func start_main_scene() -> void:
-	main = main_scene.instantiate()
-	add_child(main)
-	move_child(main, 0)
+	_main = _main_scene.instantiate()
+	add_child(_main)
+	move_child(_main, 0)
+	_hide_loading_screen()
 
 
 func stop_main_scene() -> void:
-	if main:
-		main.queue_free()
-		remove_child(main)
+	if _main:
+		_main.queue_free()
+		remove_child(_main)
