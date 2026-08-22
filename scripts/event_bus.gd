0e3521cc0cea50161862a061a3e8537926cc1541
rfarr <9212000+fazzamatazz@users.noreply.github.com>
2026-08-22 11:18:45 +0100

    suppress unhelpful unused signal warnings but only in event bus singleton

--- scripts/event_bus.gd
+++ scripts/event_bus.gd
@@ -1,5 +1,7 @@
 extends Node
 
+@warning_ignore_start("unused_signal")
+
 signal settings_invert_mouse(toggled_on: bool)
 signal settings_invert_gamepad(toggled_on: bool)
 signal settings_change_mouse_sensitivity(value: float)
@@ -9,3 +11,5 @@ signal open_settings_menu()
 signal open_pause_menu()
 signal pause_game()
 signal resume_game()
+
+@warning_ignore_restore("unused_signal")
